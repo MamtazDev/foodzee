281c9a64683f6622f62f918c2ba6207ca0c1d173
MamtazDev <akternipa941@gmail.com>
2023-11-24 12:15:58 +0600

    add modal add

---
+++ src/Components/Modal/AddModal.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import googlePlay from "../../assets/google-play.png";
+import appStore from "../../assets/appstore.png";
+import qr from "../../assets/qr.png";
+import download from "../../assets/download.png";
+import close from "../../assets/close-modal.png";
+
+const AddModal = ({ modalRef }) => {
+  return (
+    <div className="add_modal">
+      <button
+        ref={modalRef}
+        type="button"
+        className="btn btn-primary d-none"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-xl">
+          <div className="modal-content">
+            <div className="modal-body">
+              <div className="row m-0">
+                <div className="col-7 pt-5 pb-1 ps-5">
+                  <h1>
+                    <span>Download the Foodzee App</span> - <br /> Your Gateway
+                    to Savings
+                  </h1>
+                  <p>
+                    Are you ready to embark on a transformative journey filled
+                    with savings and sustainable dining experiences? Look no
+                    further – download the Foodzee app on your Android or iOS
+                    device and unlock a world of possibilities that redefine the
+                    way you dine.
+                  </p>
+                  <div className="d-flex align-items-center gap-4">
+                    <img src={qr} alt="" />
+                    <div className="d-flex flex-column gap-2 align-items-center">
+                      <img src={googlePlay} alt="" />
+                      <img src={appStore} alt="" />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-5 download text-end p-0">
+                  <button data-bs-dismiss="modal">
+                    <img className="p-2" src={close} alt="" />
+                  </button>
+
+                  <img className="img-fluid" src={download} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddModal;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Banner from "../Components/Home/Banner";
 import Quarter from "../Components/Home/Quarter";
 import Sustainability from "../Components/Home/Sustainability";
@@ -7,18 +7,30 @@ import Dining from "../Components/Home/Dining";
 import Discover from "../Components/Home/Discover";
 import Opportunity from "../Components/Home/Opportunity";
 import JoinPower from "../Components/Home/JoinPower";
+import AddModal from "../Components/Modal/AddModal";
 
 const Home = () => {
+  const modalRef = useRef();
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (modalRef.current) {
+        modalRef.current.click();
+      }
+    }, 1000);
+    return () => clearTimeout(timeoutId);
+  }, [modalRef]);
+
   return (
     <div>
+      <AddModal modalRef={modalRef} />
       <Banner />
       <Quarter />
       <Sustainability />
       <Impact />
       <Dining />
-      <Discover/>
-      <Opportunity/>
-      <JoinPower/>
+      <Discover />
+      <Opportunity />
+      <JoinPower />
     </div>
   );
 };
