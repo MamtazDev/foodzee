11826a3ce0157ef464372134d6dee11b9e9851c6
MamtazDev <akternipa941@gmail.com>
2023-11-29 10:12:23 +0600

    update

--- src/Components/Home/Quarter.jsx
+++ src/Components/Home/Quarter.jsx
@@ -5,6 +5,8 @@ import combat from "../../assets/combact.png";
 import combatHov from "../../assets/combat-hov.png";
 import tastegood from "../../assets/tastegood.png";
 import tastegoodHov from "../../assets/tastegood-hov.png";
+import "react-multi-carousel/lib/styles.css";
+import Carousel from "react-multi-carousel";
 
 const Quarter = () => {
   const list = [
@@ -30,13 +32,32 @@ const Quarter = () => {
         "Indulge in flavorful meals while making a positive impact by supporting our commitment to reducing food waste and promoting sustainability.",
     },
   ];
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <div className="quarter">
       <div className="container">
         <h2>
           Over <span>Three-Quarter</span> Of All Food Is Wasted
         </h2>
-        <div className="row">
+        <div className="row d-none d-md-block">
           {list.map((data, index) => (
             <div className="col-12 col-md-6 col-lg-4" key={index}>
               <div className="quarter_card d-flex align-items-center flex-column h-100">
@@ -50,6 +71,28 @@ const Quarter = () => {
               </div>
             </div>
           ))}
+        </div>
+        <div className="d-block d-md-none">
+          <Carousel
+            arrows={false}
+            showDots={true}
+            responsive={responsive}
+            className="pb-5 "
+          >
+            {list.map((data, index) => (
+              <div className="" key={index}>
+                <div className="quarter_card d-flex align-items-center flex-column h-100">
+                  <img className="default" src={data.pic} alt="" />
+                  <img className="hov" src={data.picHov} alt="" />
+
+                  <div className="flex-grow-1">
+                    <h3>{data.title}</h3>
+                    <p>{data.subtitle}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Carousel>
         </div>
       </div>
     </div>
